e431c51e43d66229cedc69f790bab2adc9c2f1bb
Austin <austindoq@gmail.com>
2026-05-06 15:57:26 -0700

    Remove old login handler

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,33 +1,5 @@
 import User from "../models/user.js";
 
-async function login(req, res) {
-  try {
-    const { username, password } = req.body;
-
-    if (!username || !password) {
-      return res.status(400).send("Username and password are required");
-    }
-
-    const user = await User.findOne({ username });
-    if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).send("Invalid username or password");
-    }
-
-    req.session.regenerate((err) => {
-      if (err) {
-        console.error("Session regenerate error:", err);
-        return res.status(500).send("Server error during login");
-      }
-      req.session.userId = user._id;
-      req.session.userType = user.userType;
-      res.send(`Logged in as ${user.username}`);
-    });
-  } catch (err) {
-    console.error("Login error:", err);
-    res.status(500).send("Server error during login");
-  }
-}
-
 function logout(req, res) {
   req.session.destroy((err) => {
     if (err) {
