e8c74d2dc583b1b5801afab5ee25d71b2fdba43f
Darien <dairyprodox@gmail.com>
2026-05-06 11:09:34 -0700

    Add the navbar component

---
+++ client/src/components/site-navbar.js
@@ -0,0 +1,30 @@
+class SiteNavbar extends HTMLElement {
+  constructor() {
+    super();
+    this.renderNavbar();
+  }
+
+renderNavbar() {
+  this.innerHTML = `
+    <nav class="fixed bottom-0 left-0 right-0 z-50 bg-[#EEEEEE]" id="bottom-nav">
+      <div class="flex flex-nowrap justify-around items-center p-2">
+        <a href="" class="inline-block">
+          <img src="images/" class="h-9" alt="Home" />
+        </a>
+        <a href="" class="inline-block">
+          <img src="images/" class="h-9" alt="Map" />
+        </a>
+        <a href="" class="inline-block">
+          <img src="images/" class="h-9" alt="Recipes" />
+        </a>
+        <a href="" class="inline-block">
+          <img src="images/" class="h-9" alt="My Deals" />
+        </a>
+      </div>
+    </nav>
+  `;
+}
+
+}
+
+customElements.define("site-navbar", SiteNavbar);
