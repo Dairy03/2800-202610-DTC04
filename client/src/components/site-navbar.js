70e10d6ebc8d251d1ab7e2c639a136f578b8c9b8
Darien <dairyprodox@gmail.com>
2026-05-06 11:30:53 -0700

    Add icons to navbar

--- client/src/components/site-navbar.js
+++ client/src/components/site-navbar.js
@@ -9,16 +9,16 @@ renderNavbar() {
     <nav class="fixed bottom-0 left-0 right-0 z-50 bg-[#EEEEEE]" id="bottom-nav">
       <div class="flex flex-nowrap justify-around items-center p-2">
         <a href="" class="inline-block">
-          <img src="images/" class="h-9" alt="Home" />
+          <img src="./images/home-sprite.png" class="h-9" alt="Home" />
         </a>
         <a href="" class="inline-block">
-          <img src="images/" class="h-9" alt="Map" />
+          <img src="./images/map-sprite.png" class="h-9" alt="Map" />
         </a>
         <a href="" class="inline-block">
-          <img src="images/" class="h-9" alt="Recipes" />
+          <img src="./images/recipes-sprite.png" class="h-9" alt="Recipes" />
         </a>
         <a href="" class="inline-block">
-          <img src="images/" class="h-9" alt="My Deals" />
+          <img src="./images/my-deals-sprite.png" class="h-9" alt="My Deals" />
         </a>
       </div>
     </nav>
