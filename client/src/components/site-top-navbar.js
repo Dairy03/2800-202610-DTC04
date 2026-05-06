e3d0ccd7d9f8a8e72db0b068d6d5f58838ab41aa
Darien <dairyprodox@gmail.com>
2026-05-06 13:25:09 -0700

    Create top navbar, update style links in html pages

---
+++ client/src/components/site-top-navbar.js
@@ -0,0 +1,23 @@
+class SiteTopNavbar extends HTMLElement {
+  constructor() {
+    super();
+    this.renderTopNavbar();
+  }
+
+renderTopNavbar() {
+  this.innerHTML = `
+    <nav class="fixed top-0 left-0 right-0 z-50 bg-[#EEEEEE]" id="top-nav">
+      <div class="flex flex-nowrap justify-between items-center p-2">
+        <img src="./images/profile-sprite.png" class="h-9 invisible"/>
+        <h1 class="font-extrabold">Still Fresh</h1>
+        <a href="" class="inline-block">
+          <img src="./images/profile-sprite.png" class="h-9" alt="Recipes" />
+        </a>
+      </div>
+    </nav>
+  `;
+}
+
+}
+
+customElements.define("site-top-navbar", SiteTopNavbar);
