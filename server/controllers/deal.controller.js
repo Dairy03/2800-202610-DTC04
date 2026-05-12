e05aa5abb2d28215a0d20ed10b4831879c909df8
Austin <austindoq@gmail.com>
2026-05-12 13:59:15 -0700

    Add accept deal and update cart routes/controller

--- server/controllers/deal.controller.js
+++ server/controllers/deal.controller.js
@@ -20,14 +20,57 @@ async function acceptDeal(req, res) {
   }
 }
 
-async function removeDeal(req, res) {
-  itemId = req.params.itemId;
+async function updateCart(req, res) {
+  const { itemId, quantity } = req.params;
+  const changeAmount = Number(quantity);
+  const userId = req.session.userId;
+  //Find the users current cart entry for the item, and get current quantity.
   try {
-    console.log(`Removed item ID: ${itemId}`);
+    const user = await User.findOne({ _id: userId, "cart.itemId": itemId });
+    const cartEntry = user.cart.find(
+      (item) => item.itemId.toString() === itemId,
+    );
+    const newQuantity = cartEntry.quantity + changeAmount;
+
+    //Remove Item from cart if 0 quantity
+    if (newQuantity < 1) {
+      await User.findOneAndUpdate(userId, {
+        $pull: { cart: { itemId: itemId } },
+      });
+      return res.status(200).json({
+        success: true,
+        message: "Removed item with 0 quantity from cart",
+      });
+    }
+
+    //Update Cart Entry
+    await User.findOneAndUpdate(
+      { _id: userId, "cart.itemId": itemId },
+      {
+        $set: { "cart.$.quantity": newQuantity },
+      },
+    );
+    res.status(200).json({
+      success: true,
+      message: `Modified item in cart by ${quantity}.`,
+    });
   } catch (error) {
     console.log(error);
-    res.status(404).send(`Could not delete deal ID: ${dealId}.`);
+    return res
+      .status(500)
+      .json({ message: "There was an issue updating cart.", success: false });
   }
 }
 
-export { acceptDeal, removeDeal };
+// async function removeDeal(req, res) {
+//   const { itemId, quantity } = req.params.itemId;
+//   const userId = req.session.userId;
+
+//   try {
+//     res.send("A-OK!");
+//   } catch (error) {
+//     console.log(error);
+//     res.status(404).send(`Could not delete deal ID: ${dealId}.`);
+//   }
+// }
+export { acceptDeal, updateCart };
--- server/routes/deal.routes.js
+++ server/routes/deal.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/auth.js";
-import { acceptDeal, removeDeal } from "../controllers/deal.controller.js";
+import { acceptDeal, updateCart } from "../controllers/deal.controller.js";
 
 const router = Router();
 
 router.post("/accept/:itemId/:quantity", acceptDeal);
-router.delete("/remove/:itemId/:quantity", removeDeal);
+router.patch("/update/:itemId/:quantity", updateCart);
 
 export default router;
